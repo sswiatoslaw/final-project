81172d7690bc358cb4e942a1eee7e3ed11e3432c
Sviatoslav <sviatoslavsodel@gmail.com>
2021-06-21 23:51:33 +0300

    change eslint file

--- src/components/ErrorBoundary/ErrorBoundary.jsx
+++ src/components/ErrorBoundary/ErrorBoundary.jsx
@@ -1,17 +1,23 @@
-import React, {PureComponent} from 'react'
-import Error from '../Error/Error'
+import React, { PureComponent } from 'react';
+import Error from '../Error/Error';
 
 class ErrorBoundary extends PureComponent {
-    state = {
-        hasError: false
-    }
-    componentDidCatch(error, errorInfo){
-        console.log('Inside componentDidCatch', error, errorInfo);
-        this.setState({hasError: true});
-}
-    render(){
-        return this.state.hasError ? <Error/> : this.props.children;
-    }
+  constructor (props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  componentDidCatch (error, errorInfo) {
+    console.log('Inside componentDidCatch', error, errorInfo);
+    this.setState({ hasError: true });
+  }
+
+  render () {
+    const { hasError } = this.state;
+    return hasError ? <Error/> : this.props.children;
+  }
 }
 
 export default ErrorBoundary;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import ErrorBoundary from './components/ErrorBoundary';
-import {Provider} from 'react-redux';
 import store from './store/store';
 import './assets/index.scss';
 import App from './components/App';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <ErrorBoundary>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </ErrorBoundary>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
