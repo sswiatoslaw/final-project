8a9860088ec266873058c914d2be228bf6e93c4a
Горбаченко Олег <62697935+kykyshka69@users.noreply.github.com>
2021-06-24 01:26:47 +0300

    add adaptive ipad , desktop

--- src/components/JoingMailing/JoingMailing.jsx
+++ src/components/JoingMailing/JoingMailing.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import { Row, Col, Form, Input, Typography } from 'antd';
+import { Row, Col, Form, Input } from 'antd';
 import { RightOutlined } from '@ant-design/icons';
 import validateFormJoingMailing from './validateFormJoingMailing'
-const { Title } = Typography;
 
 const JoingMailing = () => {
   return (
     <Row className={'our-mailing-list'}>
-    <Col>
-    <Title level={3} className={'our-mailing-list_text'}>Join Our Mailing List</Title>
+    <Col className={'our-mailing-list_col'}>
+    <h3 className={'our-mailing-list_text'}>Join Our Mailing List</h3>
         <Form name="send-mail" validateMessages={validateFormJoingMailing}>
       <Form.Item name={'email'} rules={[{ type: 'email' }]}>
         <Input placeholder={'Enter your @mail.com'} suffix={<RightOutlined onClick={() => {}}/>}/>
