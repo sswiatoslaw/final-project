630025ea377e88775117a515bbd7fe9f27ca9f75
Igor Bul <exeleroon@gmail.com>
2021-06-24 19:12:03 +0300

    category refresh

--- src/index.js
+++ src/index.js
@@ -5,25 +5,25 @@ import ErrorBoundary from './components/ErrorBoundary';
 import store from './store/store';
 import './assets/index.scss';
 import App from './components/App';
-import Categories from './components/Categories/Categories';
+// import Categories from './components/Categories/Categories';
 // import 'antd/dist/antd.css';
-import { Row } from 'antd';
-import pic from './assets/images/shoes&socks/bows.jpg';
+// import { Row } from 'antd';
+// import pic from './assets/images/shoes&socks/bows.jpg';
 import './components/Categories/Categories.scss';
 
-const categoriesArr = [
-  {
-    text: 'one',
-    url: pic
-  },
-  {
-    text: 'two',
-    url: pic
-  },
-  {
-    text: 'three',
-    url: pic
-  }];
+// const categoriesArr = [
+//   {
+//     text: 'one',
+//     url: pic
+//   },
+//   {
+//     text: 'two',
+//     url: pic
+//   },
+//   {
+//     text: 'three',
+//     url: pic
+//   }];
 
 ReactDOM.render(
   <React.StrictMode>
