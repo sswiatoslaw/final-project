142d514f7bf382320917eb8daa441c77159aaa70
Sviatoslav <sviatoslavsodel@gmail.com>
2021-06-26 17:40:33 +0300

    edit

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Modal, Form, Input } from 'antd'
+import { Modal, Form, Input, Checkbox, Button } from 'antd'
 import { toggle_isModalOpen } from '../store/modal/actions';
 import { Formik } from 'formik'
 const LoginPage = () => {
@@ -9,41 +9,88 @@ const LoginPage = () => {
   const toggleModal = () => {
     dispatch(toggle_isModalOpen())
   };
+  const toggleIsAdmin = (e) => {
+    console.log(e.target.checked)
+  }
+
+  const handleSubmit = (values) => {
+    console.log(values.firstName)
+  }
   return (
-    <div>
-      <Modal title="Login" visible={isModalOpen} onCancel={toggleModal}>
-          <Formik>
-            {props => (
+          <Formik
+            initialValues={{
+              firstName: '123',
+              lastName: '',
+              email: '',
+              login: '',
+              password: '',
+              isAdmin: true
+            }}
+            onSubmit={handleSubmit}
+          >
+            {(values, handleChange) => (
+                <Modal
+                    title="Login"
+                    centered="true"
+                    visible={isModalOpen}
+                    footer={[
+                        <>
+                            <Button key="button" onClick={toggleModal}>
+                                Cancel
+                            </Button>
+                            <Button htmlType="submit" type="submit">
+                                Submit
+                            </Button>
+                        </>
+                    ]}
+                >
+
                 <Form
+                    name='register'
                     initialValues={{ remember: true }}
                 >
                     <Form.Item
-                        label="Email"
-                        name="Email"
-                        rules={[{required: true, messeage: 'Please input your e-mail!'}]}
+                        label="FirstName"
+                        name="firstName"
+                        rules={[{required: true, messeage: 'Please input your firstName!'}]}
+                    >
+                        <Input value={values.firstName} onChange={handleChange} name='firstName'/>
+                    </Form.Item>
+                    <Form.Item
+                        label="LastName"
+                        name="lastName"
+                        rules={[{required: true, messeage: 'Please input your lastName!'}]}
                     >
                         <Input />
                     </Form.Item>
                     <Form.Item
                         label="Email"
-                        name="Email"
+                        name="email"
                         rules={[{required: true, messeage: 'Please input your e-mail!'}]}
                     >
                         <Input />
                     </Form.Item>
                     <Form.Item
-                        label="Email"
-                        name="Email"
-                        rules={[{required: true, messeage: 'Please input your e-mail!'}]}
+                        label="Login"
+                        name="login"
+                        rules={[{required: true, messeage: 'Please input your login!'}]}
                     >
                         <Input />
                     </Form.Item>
-
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{required: true, messeage: 'Please input your password!'}]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Checkbox name="isAdmin" onChange={toggleIsAdmin}>
+                        Admin?
+                    </Checkbox>
                 </Form>
+                </Modal>
             )}
           </Formik>
-      </Modal>
-    </div>
   );
 };
 
