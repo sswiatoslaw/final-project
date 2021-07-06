1ee5e4af3c2a671e64c0dfd7dd8c620287318655
Viktoria-ft <viktoria.viptour@gmail.com>
2021-07-06 10:42:58 +0300

    correct footer

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import ErrorBoundary from './components/ErrorBoundary';
 import store from './store/store';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 // import './assets/index.scss';
 // import Header from './components/Header';
 // import ShopPage from './pages/ShopPage';
