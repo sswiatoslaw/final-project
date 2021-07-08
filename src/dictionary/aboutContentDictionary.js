b7fed977128029f57c000df10e6508cc2772b9f3
irynaMatlayeva <iryna.matlayeva@gmail.com>
2021-07-08 23:59:36 +0300

    add About container

---
+++ src/dictionary/aboutContentDictionary.js
@@ -0,0 +1,62 @@
+const aboutImgFirst = 'https://savvy-tots.s3.eu-west-3.amazonaws.com/about-the-history.png';
+const aboutImgSecond = 'https://savvy-tots.s3.eu-west-3.amazonaws.com/about-the-history-2.png';
+
+const aboutHistoryDesc = 'Our Founder, Ying Liu';
+const aboutHistoryTitle = 'The History Behind Kyte Baby';
+const aboutHistoryText = ['Babies have always been a major part of my life. My first job was as an interpreter and tour guide for American adoption' +
+'groups in China while I was a first-year English student at a university. I would accompany groups of adoptive parents to orphanages to pick up their' +
+' children. Following this, I’d bring them to local authorities & consulates to complete their paperwork. Nothing is more beautiful than the outpouring' +
+'of love & joy on the parents’ faces when they first see their long-awaited babies. That feeling really sticks with you.', 'Ultimately, I left China to pursue a degree in Fine Arts and Economics in Canada, resulting in my having a PhD in Economics. After graduate school, I' +
+'started working as an economist for the central bank of Canada. After the birth of our first child, I found it harder and  harder to leave my newborn' +
+'with caregivers to go to work. I wanted to see him eat every meal, and I took & developed pictures of him while he took his naps. I needed a career that' +
+'would allow me to free up time so I could be there for all the big moments in my baby’s life that I otherwise would’ve missed. I quit my job at the bank' +
+'and opened up a retail store selling quality baby products. After years of sleeplessness, the baby retail business grew into a chain of five stores, and' +
+'our family grew to seven, with me, my husband, and our five babies.'
+];
+
+const aboutProblemDesc = 'Mar 13, 2021';
+const aboutProblemTitle = 'The Original Problem';
+const aboutProblemText = ['One of my daughters has chronic eczema. At night, she would pull open her pajamas to stay cool, which seemed to calm her from' +
+'constant scratching. I started to research, looking for fabrics that would keep her skin cool, and also wouldn’t irritate her. Turns out,' +
+'that fabric is bamboo.', 'Bamboo is 3 degrees cooler than cotton, while also being hypoallergenic. I started putting bamboo clothing and swaddles on' +
+'her. The softness & coolness of the fabric are unmatched by cotton, or anything else for that matter. She immediately stopped pulling' +
+'off her clothes, and her sleep significantly improved. I also found out that bamboo is a highly sustainable resource. It requires much less time' +
+'& water to grow bamboo compared to cotton, and it practically uses no pesticides. It fit every criteria I was looking for in a fabric.' +
+'With that choice made, Kyte BABY was born.', 'The name Kyte BABY was chosen very purposefully. Playing on the whimsical activity of flying a kite, Kyte is meant to evoke a' +
+'feeling of innocence, freedom, and a return to nature. All of these ideas carry over into our core business values as well. Kyte BABY is testament' +
+' to timeless fun and youth, while being natural and healthy.'
+];
+
+const aboutSolutionTitle = 'Our Solution';
+const aboutSolutionText = ['When we visited our family for a reunion in China, we told them about our business ideas. Aunt Ling, who owns a garment' +
+'factory in a small town in Southern China, suggested that I start my own line of bamboo sleepwear, sourced in China. I never saw Aunt Ling much' +
+'growing up, as she was always busy running her factory and would only return home during the New Year. Over the past 30 years, she worked her way up' +
+'from a sewing girl to the owner of a factory that is certified to export clothing to top European lines. However, her factory was not getting enough' +
+'work lately because of rising labor costs and tougher competition from other Asian countries. One of the biggest problems she faced was that she' +
+'couldn’t afford to keep all the workers during off-peak seasons. These are the workers that she eats every meal with and sleeps in the same dorm' +
+'building with. She asked, “Why don’t you design your own line of bamboo baby clothing, and I will manufacture' +
+'them for you, during the factory’s down times?” She added, “Babies are born year-round, right?”', 'Then it all came together, and I was able to start a family-owned & operated company that went all the way to sourcing. I’d get to pursue\n' +
+'my artist’s dream, and she gets to keep the factory afloat. Just like using bamboo fabric itself, filling the factory’s down time truly promotes\n' +
+'the smart use of resources and sustainability. This way of thinking is evident in every aspect of Kyte BABY.'
+];
+
+export const articles = [
+  {
+    img: aboutImgFirst,
+    altText: 'Founders',
+    desc: aboutHistoryDesc,
+    title: aboutHistoryTitle,
+    text: aboutHistoryText
+  },
+  {
+    img: aboutImgSecond,
+    altText: 'Founders',
+    desc: aboutProblemDesc,
+    title: aboutProblemTitle,
+    text: aboutProblemText
+  },
+  {
+    title: aboutSolutionTitle,
+    text: aboutSolutionText
+  }
+];
