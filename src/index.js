cc1ffd3a27152f1d9d7e1cf20789580c40959e7f
Igor Bul <exeleroon@gmail.com>
2021-07-12 19:35:09 +0300

    finally category with pictures

--- src/index.js
+++ src/index.js
@@ -5,30 +5,17 @@ import ErrorBoundary from './components/ErrorBoundary';
 import store from './store/store';
 import './assets/index.scss';
 import App from './components/App';
-// import Categories from './components/Categories/Categories';
-// import 'antd/dist/antd.css';
-// import { Row } from 'antd';
-// import pic from './assets/images/shoes&socks/bows.jpg';
+import Categories from './components/Categories/Categories';
+import { Row } from 'antd';
 import './components/Categories/Categories.scss';
-
-// const categoriesArr = [
-//   {
-//     text: 'one',
-//     url: pic
-//   },
-//   {
-//     text: 'two',
-//     url: pic
-//   },
-//   {
-//     text: 'three',
-//     url: pic
-//   }];
 
 ReactDOM.render(
   <React.StrictMode>
     <ErrorBoundary>
       <Provider store={store}>
+        <Row>
+          <Categories />
+        </Row>
         <App />
       </Provider>
     </ErrorBoundary>
