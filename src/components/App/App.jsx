abf020a8eb56c3788ea6ab9f4e776662c16dd194
Sviatoslav <72312131+sswiatoslaw@users.noreply.github.com>
2021-07-13 01:18:03 +0300

    Feature/st 22 header (#4)
    * test
    
    * SOE-1 Initial Project
    
    * add header vers. pc
    
    * fix packange.json
    
    * fix packange.json v2
    
    * fix header code
    
    * mobile version header
    
    * bug header
    
    * fix html file
    
    * final version header
    
    * add register modal
    
    * edit
    
    * final work
    
    * add use customHooks
    
    * Update index.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -18,21 +18,6 @@ function App ({ getAllProducts, allProducts }) {
     );
   });
 
-function App ({ getAllProducts, allProducts }) {
-  useEffect(() => {
-    if (allProducts.length === 0) {
-      getAllProducts();
-    }
-  }, [getAllProducts, allProducts]);
-
-  const elements = allProducts.map((item) => {
-    return (
-      <li key={item._id}>
-        {item.name}
-      </li>
-    );
-  });
-
   return (
     <div className='App'>
       <ul>
@@ -43,10 +28,10 @@ function App ({ getAllProducts, allProducts }) {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
