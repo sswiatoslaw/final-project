a9a7378f253eeed4ab189991c243bc491e2420a8
Горбаченко Олег <62697935+kykyshka69@users.noreply.github.com>
2021-07-15 22:32:21 +0300

    fake test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -5,11 +5,18 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import store from '../../store/store';
 import App from './App';
 
+window.matchMedia = window.matchMedia || function () {
+  return {
+    matches: false,
+    addListener: function () {},
+    removeListener: function () {}
+  };
+};
 it('should render App component', () => {
   render(
     <Provider store={store}>
       <Router>
-        <App />
+        <App/>
       </Router>
     </Provider>
   );
