ada7041fbe125406b8e11cdfad2430f92334ea50
irynaMatlayeva <75499440+irynaMatlayeva@users.noreply.github.com>
2021-07-15 16:01:01 +0300

    fix dev (#20)

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import Advantages from '../../components/Advantages/Advantages';
-import Baner from '../../components/Baners/Baner'
-import Button from '../../components/Button/Button';
-import GuideToFavoriteLooks from '../../components/GuideToFavoriteLooks/GuideToFavoriteLooks';
-import JoingMailing from '../../components/JoingMailing/JoingMailing';
-import {banerImagesDictionary, banerTitleDictionary, banerTextDictionary} from '../../dictionary/banerDictionary'
 
 const MainPage = () => {
   return (
-<div>
-  <Baner imgPath={banerImagesDictionary.main} imgName='woman with child' minHeight='742px' maxHeight='806px'
-         title={banerTitleDictionary.mainTitle} textBottom={banerTextDictionary.mainTextBottom} children={<Button modifier='contained' text='Shop now'
-                                                                                                       width='280px'
-                                                                                                       minWidth='335px'
-  />} />
-  <GuideToFavoriteLooks/>
-  <Advantages/>
-  <JoingMailing/>
-</div>
+    <div>
+      <h1>MAIN PAGE</h1>
+    </div>
   );
 };
 
--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import Baner from '../../components/Baners/Baner';
-import JoingMailing from '../../components/JoingMailing/JoingMailing';
-import { banerImagesDictionary, banerTextDictionary, banerTitleDictionary } from '../../dictionary/banerDictionary';
-import ProductList from '../../components/ProductList/ProductList';
 
 const ShopPage = () => {
   return (
     <div>
-      <Baner imgPath={banerImagesDictionary.shop} imgName='Baby girl shop' title={banerTitleDictionary.shopTitle}
-             textBottom={banerTextDictionary.shopTextBottom} />
-      <ProductList />
-      <JoingMailing />
+      <h1>SHOP PAGE</h1>
     </div>
   );
 };
