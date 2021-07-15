30bf8910b25a94dc1a939e2e8f3f62a7c29b1dd0
irynaMatlayeva <75499440+irynaMatlayeva@users.noreply.github.com>
2021-07-15 16:01:01 +0300

    fix dev (#20)

--- src/components/Advantages/Advantages.jsx
+++ src/components/Advantages/Advantages.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import './Advantages.scss'
-import AdvantagesContent from './AdvantegesContent/AdvantagesContent'
+import React from 'react';
+import './Advantages.scss';
+import AdvantagesContent from './AdvantegesContent/AdvantagesContent';
 import PetalsLogo from './AdvantegesContent/AdvantagesSvg/PetalsLogo';
 import RabbitLogo from './AdvantegesContent/AdvantagesSvg/RabbitLogo';
 import ChemisrtyLogo from './AdvantegesContent/AdvantagesSvg/ChemisrtyLogo';
-import { Row} from 'antd';
+import { Row } from 'antd';
+
 const Advantages = () => {
   return (
     <Row className={'advantages-block'}>
-       <h3 className={'advantages_text'}>Advantages</h3>
-       <div className={'advantages-block-logo'}>
-       <AdvantagesContent svg={<PetalsLogo/>} title={'Eco Friendly'} text={'We put together some of our favorite looks from our ss21'}/>
-       <AdvantagesContent svg={<RabbitLogo/>} title={'Comfort'} text={'Put together some of our favorite looks from our ss21'}/>
-       <AdvantagesContent svg={<ChemisrtyLogo/>} title={'Non Toxic'} text={'Together some of our favorite looks from our ss21 '}/>
-       </div>
+      <h3 className={'advantages_text'}>Advantages</h3>
+      <div className={'advantages-block-logo'}>
+        <AdvantagesContent svg={<PetalsLogo />} title={'Eco Friendly'} text={'We put together some of our favorite looks from our ss21'} />
+        <AdvantagesContent svg={<RabbitLogo />} title={'Comfort'} text={'Put together some of our favorite looks from our ss21'} />
+        <AdvantagesContent svg={<ChemisrtyLogo />} title={'Non Toxic'} text={'Together some of our favorite looks from our ss21 '} />
+      </div>
     </Row>
-  )
-}
+  );
+};
 
-export default Advantages
+export default Advantages;
--- src/components/Advantages/AdvantegesContent/AdvantagesContent.jsx
+++ src/components/Advantages/AdvantegesContent/AdvantagesContent.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import {Col} from 'antd';
-const AdvantagesContent = (props) => {
-  const {svg, text, title} = props
-  return (
-   <Col className={'advantages-content'}>
-       {svg}
-     <h5 className={'advantages-content-title'}>
-       {title}
-     </h5>
-     <p className={'content_text'}>
-        {text}
-     </p>
-   </Col>
-  )
-}
+import React from 'react';
+import { Col } from 'antd';
 
-export default AdvantagesContent
+const AdvantagesContent = (props) => {
+  const { svg, text, title } = props;
+  return (
+    <Col className={'advantages-content'}>
+      {svg}
+      <h5 className={'advantages-content-title'}>
+        {title}
+      </h5>
+      <p className={'content_text'}>
+        {text}
+      </p>
+    </Col>
+  );
+};
+
+export default AdvantagesContent;
