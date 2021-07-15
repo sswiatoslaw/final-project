c67cb716628a86ea93d8338d6a08ce2d97b57355
Igor Bul <exeleroon@gmail.com>
2021-07-15 23:00:17 +0300

    2 compontns!!!

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,13 +1,30 @@
 import { Col } from 'antd';
 import './Categories.scss';
 
-const Categories = (props) => {
-  const { text, url } = props;
-  return (
-    <Col span={8} style={{ backgroundImage: `url(${url})` }}>
-      <span className='category__wrapper'> <span className='category__text'>{text}</span> </span>
+const categoriesArr = [
+  {
+    text: 'one',
+    url: 'https://savvy-tots.s3.eu-west-3.amazonaws.com/categories/category-001.png',
+  },
+  {
+    text: 'two',
+    url: 'https://savvy-tots.s3.eu-west-3.amazonaws.com/categories/category-002.png',
+  },
+  {
+    text: 'three',
+    url: 'https://savvy-tots.s3.eu-west-3.amazonaws.com/categories/category-003.png',
+  },
+];
+
+const Categories = () => {
+  return categoriesArr.map((category) => (
+    <Col span={8} style={{ backgroundImage: `url(${category.url})`, backgroundSize: 'cover' }}>
+      <span className="category__wrapper">
+        {' '}
+        <span className="category__text">{category.text}</span>{' '}
+      </span>
     </Col>
-  );
-}
+  ));
+};
 
 export default Categories;
