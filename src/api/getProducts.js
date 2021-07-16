d49edd8b2de3d6c0631eeeecf21ea32ff1c8a4ce
Viktoria-ft <viktoria.viptour@gmail.com>
2021-07-16 09:14:28 +0300

    fix conflicts

--- src/api/getProducts.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const getProducts = async () => {
-  return await axios('https://boiling-dawn-71074.herokuapp.com/api/products');
-};
-
-export default getProducts;
--- src/store/rootreducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import modalReducer from './modal/reducer';
-import productsReducer from './products/reducer';
-
-export default combineReducers({
-  modal: modalReducer,
-  allProducts: productsReducer
-});
