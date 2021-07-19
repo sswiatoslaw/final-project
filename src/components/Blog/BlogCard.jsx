702909732101e41dd719bd4742ca793596c73847
Viktoria-ft <viktoria.viptour@gmail.com>
2021-07-19 14:46:31 +0300

    add Blog Card section for Main page

--- src/components/Blog/BlogCard.jsx
+++ src/components/Blog/BlogCard.jsx
@@ -1,31 +1,35 @@
-import img1 from './image-blog-1.png';
-import img2 from './image-blog-2.png';
-import img3 from './image-blog-3.png';
+import img1 from './blog-image-1.png';
+import img2 from './blog-image-2.png';
+import img3 from './blog-image-3.jpg';
 import './BlogCard.scss'
 
 const BlogCard = () => {
   return (
         <>
-            <div className='section-name'>BLOG</div>
             <div className='wrapper'>
-            <div className="blog">
-                <img className='blog__image' src={img1} alt='card-blog'/>
-                <div className='blog__date'>Mar 21, 2021</div>
-                <div className='blog__title'>Newborn Development: The First 12 Weeks</div>
-            </div>
 
-            <div className="blog">
-                <img className='blog__image' src={img2} alt='card-blog' />
-                <div className='blog__date'>Mar 13, 2021</div>
-                <div className='blog__title'>Newborn Sleep Schedules</div>
-            </div>
+               <div className='section-name'>BLOG</div>
+                <section className='blog'>
+            
+                    <div className='blog__card'>
+                        <img className='blog__card--image' src={img1} alt='card-blog' width='380'/>
+                        <div className='blog__card--date'>Mar 21, 2021</div>
+                        <div className='blog__card--title'>Newborn Development: The First 12 Weeks</div>
+                    </div>
 
-            <div className="blog">
-                <img className='blog__image' src={img3} alt='card-blog' />
-                <div className='blog__date'>Mar 07, 2021</div>
-                <div className='blog__title'>International Women's Day Q&A With Ying Liu</div>
+                    <div className='blog__card'>
+                        <img className='blog__card--image' src={img2} alt='card-blog' />
+                        <div className='blog__card--date'>Mar 13, 2021</div>
+                        <div className='blog__card--title'>Newborn Sleep Schedules</div>
+                    </div>
+
+                    <div className='blog__card'>
+                        <img className='blog__card--image' src={img3} alt='card-blog' />
+                        <div className='blog__card--date'>Mar 07, 2021</div>
+                        <div className='blog__card--title'>International Women's Day Q&A With Ying Liu</div>
+                    </div>
+                </section>
             </div>
-        </div>
         </>
 
   )
