8af5cf2889cfac340a96ccc8563b0d5de4fc2f51
irynaMatlayeva <iryna.matlayeva@gmail.com>
2021-07-25 22:21:31 +0300

    create shopping cart component, add quantity, cart to redux

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,15 +4,16 @@ import MenuComponent from './../Menu';
 import { Link } from 'react-router-dom';
 import { HeartOutlined, ShoppingOutlined, UserOutlined } from '@ant-design/icons';
 import { toggle_isModalOpen } from '../../store/modal/actions';
-import { useDispatch } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import useWindowSize from '../../сustomHooks/useWindowSize';
 
-function Header () {
+function Header ({ cart }) {
   const dispatch = useDispatch();
   const size = useWindowSize();
   const toggleModal = () => {
     dispatch(toggle_isModalOpen());
   };
+
   return (
     <>
       <div className='header'>
@@ -29,7 +30,7 @@ function Header () {
             <HeartOutlined style={{ fontSize: '24px', color: '#A8D6CB' }} />
           </Link>
           <Link to='/cart'>
-            <ShoppingOutlined style={{ fontSize: '24px', color: '#A8D6CB' }} />
+            <ShoppingOutlined style={{ fontSize: '24px', color: '#A8D6CB' }} /><sup className='header__sup-text'>{cart.length}</sup>
           </Link>
           <Link to='/login'>
             <UserOutlined onClick={toggleModal} style={{ fontSize: '24px', color: '#A8D6CB' }} />
@@ -43,4 +44,10 @@ function Header () {
   );
 }
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    cart: state.cart
+  };
+};
+
+export default connect(mapStateToProps)(Header);
