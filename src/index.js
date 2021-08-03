2e00ebcd2f136c21c3a64fa44d6a78cc10025c0a
Viktoria-ft <viktoria.viptour@gmail.com>
2021-08-04 00:16:56 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import ErrorBoundary from './components/ErrorBoundary';
 import store from './store/store';
 import { BrowserRouter } from 'react-router-dom';
 import './assets/index.scss';
+import App from './components/App/index';
 
 ReactDOM.render(
   <React.StrictMode>
     <ErrorBoundary>
       <Provider store={store}>
         <BrowserRouter>
-                   
+        <App/>
         </BrowserRouter>
       </Provider>
     </ErrorBoundary>
