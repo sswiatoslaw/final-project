2a77896da2406813774f2851ec01cef879888a4c
irynaMatlayeva <iryna.matlayeva@gmail.com>
2021-08-05 13:24:46 +0300

    refactoring

--- src/pages/BlogPage/BlogPage.jsx
+++ src/pages/BlogPage/BlogPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {banerImagesDictionary, banerTitleDictionary} from '../../dictionary/banerDictionary';
+import { banerImagesDictionary, banerTitleDictionary } from '../../dictionary/banerDictionary';
 import Baner from '../../components/Baners/Baner';
 import JoingMailing from '../../components/JoingMailing/JoingMailing';
 import BlogContainer from './../../containers/BlogContainer/index';
@@ -8,11 +8,10 @@ import './BlogPage.scss'
 const BlogPage = () => {
   return (
     <div>
-  <Baner imgPath={banerImagesDictionary.blog} imgName='girls' title={banerTitleDictionary.blogTitle}/>
-  <BlogContainer/>
-  <JoingMailing/>
- 
-</div>
+      <Baner imgPath={banerImagesDictionary.blog} imgName='girls' title={banerTitleDictionary.blogTitle}/>
+      <BlogContainer/>
+      <JoingMailing/>
+    </div>
   )
 }
 
