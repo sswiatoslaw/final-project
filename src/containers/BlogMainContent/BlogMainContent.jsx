b3b274ea169722362b07dbe335b58503e68dab66
Viktoria-ft <viktoria.viptour@gmail.com>
2021-08-11 22:52:37 +0300

    fix conflicts

--- src/containers/BlogMainContent/BlogMainContent.jsx
+++ src/containers/BlogMainContent/BlogMainContent.jsx
@@ -2,10 +2,7 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import fetchSlides from '../../store/slides/actions';
 import BlogMain from '../../components/BlogMain/BlogMain';
-<<<<<<< HEAD
 import './BlogMainContent.scss';
-=======
->>>>>>> 112ce4acf7c65da4e4fb73080e5050861aeec590
 
 function BlogMainContent ({ getAllSlides, allSlides }) {
   useEffect(() => {
@@ -15,17 +12,11 @@ function BlogMainContent ({ getAllSlides, allSlides }) {
   }, [getAllSlides, allSlides]);
 
   return (
-<<<<<<< HEAD
     <div className='wrapper'>
       <h2 className='blog__title'>BLOG</h2>
       <BlogMain />
 
     </div>
-=======
-    <>
-     <BlogMain />
-    </>
->>>>>>> 112ce4acf7c65da4e4fb73080e5050861aeec590
   );
 };
 
