dc17533cab0b3afeaa836f18104a65b41f856f95
Viktoria-ft <69361100+Viktoria-ft@users.noreply.github.com>
2021-08-15 23:16:06 +0300

    St 94 pagination (#45)
    * add card
    
    * blog mobile
    
    * add Blog Card section for Main page
    
    * add images for blog
    
    * rename images
    
    * Blog list, blog container and blod main list are ready
    
    * fixed conflicts
    
    * without logo
    
    * fixed conflicts
    
    * add blog container to blog page
    
    * add pagination
    
    * add styles for product
    
    * add pagination for products
    
    * fix conflicts
    
    * add isLoading and correct pagination style
    
    * fix conflicts
    
    * after conflicts
    
    * change productList on productContent on Shop psge

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,18 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import Product from '../Product/Product';
 import { addFavoritesAction, addItemToFavoriteAction, removeItemFromFavoriteAction } from '../../store/favorite/actions';
+import Loading from '../Loading/Loading';
 import { notification } from 'antd';
 import './ProductList.scss';
 
-const ProductList = ({allProducts, favorite, addFavoritesAction, addItemToFavoriteAction, removeItemFromFavoriteAction}) => {
-  const [products, setProducts] = useState([]);
-
+const ProductList = ({ allProducts, favorite, addFavoritesAction, addItemToFavoriteAction, removeItemFromFavoriteAction, products, setProducts}) => {
   useEffect(() => {
     const localStoreFromFavorite = localStorage.getItem('favorite') === null ? [] : Array.from(JSON.parse(localStorage.getItem('favorite')))
     if (localStoreFromFavorite.length !== 0) {
       addFavoritesAction(localStoreFromFavorite)
     }
   }, [addFavoritesAction])
+
+  const [isLoading, setLoading] = useState(true)
 
   useEffect(() => {
     setProducts(() => {
@@ -22,10 +23,14 @@ const ProductList = ({allProducts, favorite, addFavoritesAction, addItemToFavori
         if (!productsNameList.find(el => el.name === item.name)) {
           productsNameList.push(item)
         }
+        setLoading(false)
       })
       return productsNameList
     })
-  }, [allProducts]);
+  }, [allProducts, setProducts])
+  if (isLoading) {
+    return <Loading/>
+  }
 
   const openErrorNotification = () => {
     notification.error({
