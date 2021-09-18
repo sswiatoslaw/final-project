dc4790e0a5b78961903a4862e1d16eebf53203bf
Viktoria-ft <viktoria.viptour@gmail.com>
2021-09-18 21:38:16 +0300

    fix some styles

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -33,7 +33,7 @@ const Product = ({ product, addToCart, onToggleImportant, favorite }) => {
               <Rater/>
             </span>
           </div>
-          <Button className='card__button' onClick={ () => addItemToCart(product) }>To bag - $ { product.currentPrice } </Button>
+          <Button className='card__button' onClick={ () => addItemToCart(product._id) }>To bag - $ { product.currentPrice } </Button>
 
         </Card>
       </li>
