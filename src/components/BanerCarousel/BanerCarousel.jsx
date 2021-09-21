e00a2765e64feaff20687e4433c58893302d7b88
sSodel <sviatoslav.sodel@creditkasa.ua>
2021-09-21 22:03:17 +0300

    final admin

--- src/components/BanerCarousel/BanerCarousel.jsx
+++ src/components/BanerCarousel/BanerCarousel.jsx
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
-import Baner from '../Baners/Baner';
-import { banerImagesDictionary, banerTextDictionary, banerTitleDictionary } from '../../dictionary/banerDictionary'
+import Banner from '../Banners/Banner';
+import { bannerImagesDictionary, bannerTextDictionary, bannerTitleDictionary } from '../../dictionary/bannerDictionary';
 import Button from '../../components/Button/Button';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
@@ -12,18 +12,18 @@ const BanerCarousel = () => {
       <div>
        
            <Carousel>
-            <Baner imgPath={ banerImagesDictionary.main } imgName='woman with child' minHeight='742px' maxHeight='806px'
-             title={ banerTitleDictionary.mainTitle } textBottom={ banerTextDictionary.mainTextBottom } children={ <Button modifier='contained' text='Shop now'
+            <Banner imgPath={ bannerImagesDictionary.main } imgName='woman with child' minHeight='742px' maxHeight='806px'
+             title={ bannerTitleDictionary.mainTitle } textBottom={ bannerTextDictionary.mainTextBottom } children={ <Button modifier='contained' text='Shop now'
                                                                                                                            width='280px'
                                                                                                                            minWidth='335px'
       /> }/>
-            <Baner imgPath={ banerImagesDictionary.main } imgName='woman with child' minHeight='742px' maxHeight='806px'
-             title={ banerTitleDictionary.mainTitle } textBottom={ banerTextDictionary.mainTextBottom } children={ <Button modifier='contained' text='Shop now'
+            <Banner imgPath={ bannerImagesDictionary.main } imgName='woman with child' minHeight='742px' maxHeight='806px'
+             title={ bannerTitleDictionary.mainTitle } textBottom={ bannerTextDictionary.mainTextBottom } children={ <Button modifier='contained' text='Shop now'
                                                                                                                            width='280px'
                                                                                                                            minWidth='335px'
       /> }/>
-            <Baner imgPath={ banerImagesDictionary.main } imgName='woman with child' minHeight='742px' maxHeight='806px'
-             title={ banerTitleDictionary.mainTitle } textBottom={ banerTextDictionary.mainTextBottom } children={ <Button modifier='contained' text='Shop now'
+            <Banner imgPath={ bannerImagesDictionary.main } imgName='woman with child' minHeight='742px' maxHeight='806px'
+             title={ bannerTitleDictionary.mainTitle } textBottom={ bannerTextDictionary.mainTextBottom } children={ <Button modifier='contained' text='Shop now'
                                                                                                                            width='280px'
                                                                                                                            minWidth='335px'
       /> }/>
--- src/pages/AdminPage/components/Topbar/Topbar.jsx
+++ src/pages/AdminPage/components/Topbar/Topbar.jsx
@@ -5,6 +5,7 @@ import { Divider, Drawer, List, ListItem, ListItemText } from '@material-ui/core
 import Avatar from '../Avatar'
 import axios from 'axios'
 import { Link } from 'react-router-dom';
+import { Button, Modal, Form, Input } from 'antd'
 const Topbar = () => {
   const getAvatar = () => {
     const token = localStorage.getItem('token')
@@ -18,6 +19,7 @@ const Topbar = () => {
       });
   }
   const [leftPanel, setLeftPanel] = React.useState(false);
+  const [isModalVisible, setIsModalVisible] = React.useState(false)
   const menuList = [
     {
       name: 'Products',
@@ -33,6 +35,16 @@ const Topbar = () => {
       url: '/admin/colors'
     }
   ]
+
+  const onOk = (values) => {
+    const token = localStorage.getItem('token');
+    axios.defaults.headers.common['Authorization'] = token;
+    axios.post('https://boiling-dawn-71074.herokuapp.com/api/products/', values)
+      .then(res => {
+        console.log(res)
+      })
+  }
+
   const handleLeftPanel = () => {
     setLeftPanel(!leftPanel)
   }
@@ -49,6 +61,7 @@ const Topbar = () => {
             <MenuIcon className="topbar-right__menu" onClick={handleLeftPanel} />
           </div>
           <div className="topbar-right">
+            <Button color='primary' onClick={() => { setIsModalVisible(true) }}>Add Product</Button>
             <Avatar onClick={getAvatar} />
           </div>
         </div>
@@ -70,6 +83,68 @@ const Topbar = () => {
           </List>
         </div>
       </Drawer>
+      {isModalVisible && (
+              <Modal
+              title="Add product"
+              visible={isModalVisible}
+              onCancel={() => { setIsModalVisible(false) }}
+              onOk={onOk}
+              footer={[
+                <Button color='primary' form="form" key="submit" htmlType="submit">
+                    Submit
+                </Button>
+              ]}
+            >
+              <Form
+                id='form'
+                onFinish={onOk}
+              >
+                <Form.Item
+                  label="Category"
+                  name="categories"
+                  rules={[{ required: true, message: 'Please input category!' }]}
+                >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Name"
+                name="name"
+                rules={[{ required: true, message: 'Please input name!' }]}
+              >
+              <Input />
+            </Form.Item>
+            <Form.Item
+                label="Price"
+                name="currentPrice"
+                rules={[{ required: true, message: 'Please input price!' }]}
+              >
+              <Input />
+            </Form.Item>
+            <Form.Item
+                label="Quantity"
+                name="quantity"
+                rules={[{ required: true, message: 'Please input quantity!' }]}
+              >
+              <Input />
+            </Form.Item>
+            <Form.Item
+                label="Size"
+                name="sizes"
+                rules={[{ required: true, message: 'Please input size!' }]}
+              >
+              <Input />
+            </Form.Item>
+            <Form.Item
+                label="Color"
+                name="color"
+                rules={[{ required: true, message: 'Please input color!' }]}
+              >
+              <Input />
+            </Form.Item>
+      
+              </Form>
+            </Modal>
+      )}
     </>
   )
 }
