d49428f78f55a53dbbc586c815981da871169c91
Viktoria-ft <viktoria.viptour@gmail.com>
2021-09-28 08:37:05 +0300

    fixed conflicts

--- src/pages/BlogPage/BlogPage.jsx
+++ src/pages/BlogPage/BlogPage.jsx
@@ -1,14 +1,6 @@
 import React from 'react'
-<<<<<<< HEAD
 import { bannerImagesDictionary, bannerTitleDictionary } from '../../dictionary/bannerDictionary';
 import Banner from '../../components/Banners/Banner';
-||||||| 01e9988
-import { banerImagesDictionary, banerTitleDictionary } from '../../dictionary/banerDictionary';
-import Baner from '../../components/Baners/Baner';
-=======
-import { bannerImagesDictionary, bannerTitleDictionary } from '../../dictionary/banerDictionary';
-import Banner from '../../components/Banners/Banner';
->>>>>>> 093e7e2629187c565eaacba5c5df474aa40bd5ca
 import JoingMailing from '../../components/JoingMailing/JoingMailing';
 import BlogContainer from './../../containers/BlogContainer/index';
 import './BlogPage.scss'
--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 import './ShopPage.scss'
 import Banner from '../../components/Banners/Banner';
 import JoingMailing from '../../components/JoingMailing/JoingMailing';
-<<<<<<< HEAD
 import { bannerImagesDictionary, bannerTextDictionary, bannerTitleDictionary } from '../../dictionary/bannerDictionary';
-||||||| 01e9988
-import { banerImagesDictionary, banerTextDictionary, banerTitleDictionary } from '../../dictionary/banerDictionary';
-=======
-import { bannerImagesDictionary, bannerTextDictionary, bannerTitleDictionary } from '../../dictionary/banerDictionary';
->>>>>>> 093e7e2629187c565eaacba5c5df474aa40bd5ca
 import FilterResponsive from '../../components/Filter/FilterResponsive/FilterResponsive';
 import ProductList from '../../components/ProductList/ProductList';
 import Sort from '../../components/Sort/Sort';
