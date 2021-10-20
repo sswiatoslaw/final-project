8b2d4580e6feaa4faea134120d16a54333e8bdb5
Viktoria-ft <viktoria.viptour@gmail.com>
2021-10-20 23:46:06 +0300

    correct styles

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
 import Banner from '../../components/Banners/Banner';
 import JoingMailing from '../../components/JoingMailing/JoingMailing';
 import { bannerImagesDictionary, bannerTextDictionary, bannerTitleDictionary } from '../../dictionary/bannerDictionary';
-// import FilterResponsive from '../../components/Filter/FilterResponsive/FilterResponsive';
-// import ProductList from '../../components/ProductList/ProductList';
-// import Sort from '../../components/Sort/Sort';
-// import { Layout } from 'antd';
-// import useWindowSize from '../../сustomHooks/useWindowSize';
+import FilterResponsive from '../../components/Filter/FilterResponsive/FilterResponsive';
+import Sort from '../../components/Sort/Sort';
+import { Layout } from 'antd';
+import useWindowSize from '../../сustomHooks/useWindowSize';
 import ProductContent from './../../containers/ProductContent/ProductContent';
 
 const ShopPage = () => {
+  const size = useWindowSize();
   return (
     <div>
       <Banner imgPath={bannerImagesDictionary.shop} imgName='Baby girl shop' title={bannerTitleDictionary.shopTitle}
              textBottom={bannerTextDictionary.shopTextBottom} />
-      <ProductContent />
+             <Layout>
+             <FilterResponsive/>
+             <Layout style={{backgroundColor: 'transparent'}}>
+            {(size.width >= 769 ? <Sort/> : '')}
+               <ProductContent />
+               </Layout>
+               </Layout>
       <JoingMailing />
     </div>
   );
